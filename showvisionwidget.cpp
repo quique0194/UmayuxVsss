5d6e169a823a1a3a9e2a7db3d58a2675bc5e6ce2
José Enrique Carrillo Pino <Jos Enrique Carrillo Pino>
2016-08-19 14:37:13 -0500

    Remove unnecessary comments

--- showvisionwidget.cpp
+++ showvisionwidget.cpp
@@ -41,13 +41,11 @@ bool ShowVisionWidget::validPoint(const Point& p) {
 
 void ShowVisionWidget::paintEvent(QPaintEvent*)
 {
-    QTime time;
-    time.start();
     if (img) {
         QPainter paint(this);
         paint.drawImage(img->rect(), *img, img->rect());
         QPoint p, q;
-        int r = 20;
+        int r = 14;
         for (int i = 0; i < 3; ++i) {
             if (validPoint(my_team[i])) {
                 paint.setPen(Qt::green);
@@ -68,7 +66,6 @@ void ShowVisionWidget::paintEvent(QPaintEvent*)
             paint.drawEllipse(p, 2, 2);
         }
     }
-    cout << "TIME REPAINT " << time.elapsed() << endl;
 }
 
 vector<Point> punto_central_color(Mat imgThresholded){
@@ -146,22 +143,13 @@ void ShowVisionWidget::proc(Mat* frame) {
     QTime time;
     time.start();
     Mat imgThresholded;
+    Scalar lower_bound, upper_bound;
+
     /////////////////////////////////////////////////// AMARILLO
-    Scalar lower_bound(ch->yellowCalib.data[0], ch->yellowCalib.data[1], ch->yellowCalib.data[2]);
-    Scalar upper_bound(ch->yellowCalib.data[3], ch->yellowCalib.data[4], ch->yellowCalib.data[5]);
+    lower_bound = Scalar(ch->yellowCalib.data[0], ch->yellowCalib.data[1], ch->yellowCalib.data[2]);
+    upper_bound = Scalar(ch->yellowCalib.data[3], ch->yellowCalib.data[4], ch->yellowCalib.data[5]);
     inRange(*frame, lower_bound, upper_bound, imgThresholded); //Threshold the image
-
     vector<Point> ptos_amarillo = punto_central_color(imgThresholded);
-
-//    Mat drawing = Mat::zeros(imgThresholded.size(), CV_8UC3 );
-
-//    for (int k = 0; k<ptos_amarillo.size(); k++){
-//        //cout<<"AM_"<<k+1<<": "<<ptos_amarillo.at(k).x<<" ; "<<ptos_amarillo.at(k).y<<endl;
-//        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-//        circle( drawing, Point(ptos_amarillo.at(k).x,ptos_amarillo.at(k).y), 1, color, -1, 8, 0 );
-//    }
-
-    //imshow("Thresholded AMARILLO", imgThresholded);
 
     /////////////////////////////////////////////////// AZUL
     lower_bound = Scalar(ch->blueCalib.data[0], ch->blueCalib.data[1], ch->blueCalib.data[2]);
@@ -169,27 +157,11 @@ void ShowVisionWidget::proc(Mat* frame) {
     inRange(*frame, lower_bound, upper_bound, imgThresholded); //Threshold the image
     vector<Point> ptos_azul = punto_central_color(imgThresholded);
 
-//    for (int k = 0; k<ptos_azul.size(); k++){
-//        //cout<<"AZ_"<<k+1<<": "<<ptos_azul.at(k).x<<" ; "<<ptos_azul.at(k).y<<endl;
-//        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-//        circle( drawing, Point(ptos_azul.at(k).x,ptos_azul.at(k).y), 1, color, -1, 8, 0 );
-//    }
-
-    //imshow("Thresholded AZUL", imgThresholded);
-
-
     /////////////////////////////////////////////////// ROJO
     lower_bound = Scalar(ch->redCalib.data[0], ch->redCalib.data[1], ch->redCalib.data[2]);
     upper_bound = Scalar(ch->redCalib.data[3], ch->redCalib.data[4], ch->redCalib.data[5]);
     inRange(*frame, lower_bound, upper_bound, imgThresholded); //Threshold the image
     vector<Point> ptos_rojo = punto_central_color(imgThresholded);
-
-//    for (int k = 0; k<ptos_rojo.size(); k++){
-//        //cout<<"RO_"<<k+1<<": "<<ptos_rojo.at(k).x<<" ; "<<ptos_rojo.at(k).y<<endl;
-//        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-//        circle( drawing, Point(ptos_rojo.at(k).x,ptos_rojo.at(k).y), 1, color, -1, 8, 0 );
-//    }
-
 
     /////////////////////////////////////////////////// VERDE
     lower_bound = Scalar(ch->greenCalib.data[0], ch->greenCalib.data[1], ch->greenCalib.data[2]);
@@ -197,76 +169,40 @@ void ShowVisionWidget::proc(Mat* frame) {
     inRange(*frame, lower_bound, upper_bound, imgThresholded); //Threshold the image
     vector<Point> ptos_verde = punto_central_color(imgThresholded);
 
-//    for (int k = 0; k<ptos_verde.size(); k++){
-//        //cout<<"VE_"<<k+1<<": "<<ptos_verde.at(k).x<<" ; "<<ptos_verde.at(k).y<<endl;
-//        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-//        circle( drawing, Point(ptos_verde.at(k).x,ptos_verde.at(k).y), 1, color, -1, 8, 0 );
-//    }
-
-//    imshow("Thresholded VERDE", imgThresholded);
-
     /////////////////////////////////////////////////// CYAN
-
-    lower_bound = Scalar(ch->redCalib.data[0], ch->redCalib.data[1], ch->redCalib.data[2]);
-    upper_bound = Scalar(ch->redCalib.data[3], ch->redCalib.data[4], ch->redCalib.data[5]);
+    lower_bound = Scalar(ch->skyblueCalib.data[0], ch->skyblueCalib.data[1], ch->skyblueCalib.data[2]);
+    upper_bound = Scalar(ch->skyblueCalib.data[3], ch->skyblueCalib.data[4], ch->skyblueCalib.data[5]);
     inRange(*frame, lower_bound, upper_bound, imgThresholded); //Threshold the image
     vector<Point> ptos_cyan = punto_central_color(imgThresholded);
 
-//    for (int k = 0; k<ptos_cyan.size(); k++){
-//        //cout<<"CY_"<<k+1<<": "<<ptos_cyan.at(k).x<<" ; "<<ptos_cyan.at(k).y<<endl;
-//        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-//        circle( drawing, Point(ptos_cyan.at(k).x,ptos_cyan.at(k).y), 1, color, -1, 8, 0 );
-//    }
-
-    //imshow("Thresholded CYAN", imgThresholded);
 
     //// JUGADOR_AMARILLO_ROJO
     vector<Point> jugador_Am_Ro = punto_central(ptos_amarillo,ptos_rojo);
-    //line(drawing, jugador_Am_Ro.at(0), jugador_Am_Ro.at(1),Scalar( 110, 220, 0 ),  2, 8);
-//    circle(drawing, jugador_Am_Ro.at(2),3,Scalar( 110, 220, 0 ),  2, 8);
-    //cout<<"P1_AM_RO: "<<jugador_Am_Ro.at(2).x<<" ; "<<jugador_Am_Ro.at(2).y<<endl;
     my_team[0] = jugador_Am_Ro.at(2);
     my_team_ori[0] = jugador_Am_Ro.at(0);
 
     //// JUGADOR_AMARILLO_VERDE
     vector<Point> jugador_Am_Ve = punto_central(ptos_amarillo,ptos_verde);
-    //line(drawing, jugador_Am_Ve.at(0), jugador_Am_Ve.at(1),Scalar( 110, 220, 0 ),  2, 8);
-//    circle(drawing, jugador_Am_Ve.at(2),3,Scalar( 110, 220, 0 ),  2, 8);
-    //cout<<"P2_AM_VE: "<<jugador_Am_Ve.at(2).x<<" ; "<<jugador_Am_Ve.at(2).y<<endl;
     my_team[1] = jugador_Am_Ve.at(2);
     my_team_ori[1] = jugador_Am_Ve.at(0);
 
     //// JUGADOR_AMARILLO_CYAN
     vector<Point> jugador_Am_Cy = punto_central(ptos_amarillo,ptos_cyan);
-    //line(drawing, jugador_Am_Cy.at(0), jugador_Am_Cy.at(1),Scalar( 110, 220, 0 ),  2, 8);
-//    circle(drawing, jugador_Am_Cy.at(2),3,Scalar( 110, 220, 0 ),  2, 8);
-   // cout<<"P3_AM_CY: "<<jugador_Am_Cy.at(2).x<<" ; "<<jugador_Am_Cy.at(2).y<<endl;
     my_team[2] = jugador_Am_Cy.at(2);
     my_team_ori[2] = jugador_Am_Cy.at(0);
 
     //// JUGADOR_AZUL_ROJO
     vector<Point> jugador_Az_Ro = punto_central(ptos_azul,ptos_rojo);
-    //line(drawing, jugador_Am_Ro.at(0), jugador_Am_Ro.at(1),Scalar( 110, 220, 0 ),  2, 8);
-//    circle(drawing, jugador_Az_Ro.at(2),3,Scalar( 110, 220, 0 ),  2, 8);
-    //cout<<"P1_AZ_RO: "<<jugador_Az_Ro.at(2).x<<" ; "<<jugador_Az_Ro.at(2).y<<endl;
     op_team[0] = jugador_Az_Ro.at(2);
 
     //// JUGADOR_AZUL_VERDE
     vector<Point> jugador_Az_Ve = punto_central(ptos_azul,ptos_verde);
-    //line(drawing, jugador_Am_Ve.at(0), jugador_Am_Ve.at(1),Scalar( 110, 220, 0 ),  2, 8);
-//    circle(drawing, jugador_Az_Ve.at(2),3,Scalar( 110, 220, 0 ),  2, 8);
-   // cout<<"P2_AZ_VE: "<<jugador_Az_Ve.at(2).x<<" ; "<<jugador_Az_Ve.at(2).y<<endl;
     op_team[1] = jugador_Az_Ve.at(2);
 
     //// JUGADOR_AZUL_CYAN
     vector<Point> jugador_Az_Cy = punto_central(ptos_azul,ptos_cyan);
-    //line(drawing, jugador_Am_Cy.at(0), jugador_Am_Cy.at(1),Scalar( 110, 220, 0 ),  2, 8);
-//    circle(drawing, jugador_Az_Cy.at(2),3,Scalar( 110, 220, 0 ),  2, 8);
-   // cout<<"P3_AZ_CY: "<<jugador_Az_Cy.at(2).x<<" ; "<<jugador_Az_Cy.at(2).y<<endl;
     op_team[2] = jugador_Az_Cy.at(2);
 
-//    imgOriginal = imgOriginal + drawing;
-//    imshow("Original", imgOriginal); //show the original image
     cout << "TIME PROC " << time.elapsed() << endl;
 }
 
